5e856b7979c0ee08a28538e5154f00ac56067ea1
vinhor <88082548+vinhor@users.noreply.github.com>
2022-10-07 17:11:44 +0200

    ++ PLN

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+/* ++ = pro přidání nové měny napište x
+XXX = měna
+xxxyy = kód převodu (např.: eurkc)
+-- = zde tyto úpravy končí (pokud není ohraničeno složenou závorkou, ve které je komentář umístěn)
+if- = pokud je splněna věc v -//něco#
+"}" = něco, co již bylo napsané
+ZTML x= něco z html, x je číslo odkazu do souboru HTML
+příklad: 
+ // ++ xxxyy = new Prevod(request.response.rates.XXX);
+ // kód
+ // --
+ ale:
+  if (some) {
+    // ++ xxxyy = new Prevod(request.response.rates.XXX);
+    // kód
+  }
+ */
+
 window.onload = function () {
   let slctEvropa = document.getElementById("slctEvropa");
   let slctZbytek = document.getElementById("slctZbytek");
@@ -10,11 +28,12 @@ window.onload = function () {
   let pInput = document.getElementById("pInput");
 
   const requestURL =
-    "https://api.exchangerate.host/latest?base=CZK&symbols=EUR,USD,GBP,UAH,CHF,SEK,JPY,CAD,AUD,MXN,BRL,HRK";
+    "https://api.exchangerate.host/latest?base=CZK&symbols=EUR,USD,GBP,UAH,CHF,SEK,JPY,CAD,AUD,MXN,BRL,HRK,PLN";
   let request = new XMLHttpRequest();
   request.responseType = "json";
   request.onload = () => {
     console.log(request.response);
+    // ++ xxxyy = new Prevod(request.response.rates.XXX);
     eurkc = new Prevod(request.response.rates.EUR);
     usdkc = new Prevod(request.response.rates.USD);
     gbpkc = new Prevod(request.response.rates.GBP);
@@ -27,6 +46,7 @@ window.onload = function () {
     mxnkc = new Prevod(request.response.rates.MXN);
     brlkc = new Prevod(request.response.rates.BRL);
     hrkkc = new Prevod(request.response.rates.HRK);
+    plnkc = new Prevod(request.response.rates.PLN);
   };
   request.open("GET", requestURL);
   request.send();
@@ -97,6 +117,12 @@ window.onload = function () {
   slctEvropa.onchange = function () {
     vlevo.value = null;
     vpravo.value = null;
+    /* ++ if- že to je evropská měna# 
+    "}" else if(slctEvropa.value == ZTML 1) {
+      lvlevo.innerHTML = "ZTML 2"
+      lvpravo.innerHTML = "ZTML 3"
+    }
+    */
     if (slctEvropa.value == 0) {
       lvlevo.innerHTML = "";
       lvpravo.innerHTML = "";
@@ -118,12 +144,21 @@ window.onload = function () {
     } else if (slctEvropa.value == 6) {
       lvlevo.innerHTML = "kn";
       lvpravo.innerHTML = "Kč";
+    } else if (slctEvropa.value == 7) {
+      lvlevo.innerHTML = "zł";
+      lvpravo.innerHTML = "Kč";
     }
   };
 
   slctZbytek.onchange = function () {
     vlevo.value = null;
     vpravo.value = null;
+    /* ++ if- že to je neevropská měna# 
+    "}" else if(slctEvropa.value == ZTML 1) {
+      lvlevo.innerHTML = "ZTML 2"
+      lvpravo.innerHTML = "ZTML 3"
+    }
+    */
     if (slctZbytek.value == 0) {
       lvlevo.innerHTML = "";
       lvpravo.innerHTML = "";
@@ -150,6 +185,11 @@ window.onload = function () {
 
   function vyberEvropa() {
     upozorneni.innerHTML = "";
+    /* ++ if- v případě, že to je evropská měna#
+    "}" else if (slctEvropa.value == ZTML 1) {
+      xxxyy.meny();
+    }  
+    */
     if (slctEvropa.value == 0) {
       upozorneni.innerHTML = "Nevybrali jste si převod!";
     } else if (slctEvropa.value == 1) {
@@ -164,10 +204,17 @@ window.onload = function () {
       sekkc.meny();
     } else if (slctEvropa.value == 6) {
       hrkkc.meny();
+    } else if (slctEvropa.value == 7) {
+      plnkc.meny();
     }
   }
 
   function vyberZbytek() {
+    /* ++ if- v případě, že to je neevropská měna#
+    "}" else if (slctEvropa.value == ZTML 1) {
+      xxxyy.meny();
+    }  
+    */
     upozorneni.innerHTML = "";
     if (slctZbytek.value == 0) {
       upozorneni.innerHTML = "Nevybrali jste si převod!";
